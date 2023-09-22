c207540c298ed95c1d4694ea3dd9ef97e9a6aee1
mrlaunch <jbliu1007@gmail.com>
2023-09-22 17:55:04 -0400

    remove useless code

--- server.py
+++ server.py
@@ -1,16 +1,13 @@
 import os
-import json
 import logging
 
-from flask import Flask, request, make_response, Response
+from flask import Flask, request, make_response
 from dotenv import load_dotenv
 import pandas as pd
 
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-from slack_sdk.signature import SignatureVerifier
 
-from slashCommand import Slash
 from sentiment_analyzer import analyze_sentiment
 
 from predict_score import predict_score
@@ -20,37 +17,6 @@ load_dotenv()
 
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
-
-@app.route("/slack/test", methods=["POST"])
-def command():
-  # if not verifier.is_valid_request(request.get_data(), request.headers):
-  #   return make_response("invalid request", 403)
-  info = request.form
-
-  # # send user a response via DM
-  # im_id = slack_client.im_open(user=info["user_id"])["channel"]["id"]
-  # ownerMsg = slack_client.chat_postMessage(
-  #   channel=im_id,
-  #   text=commander.getMessage()
-  # )
-
-  # # send channel a response
-  # response = slack_client.chat_postMessage(
-  #   channel='#{}'.format(info["channel_name"]), 
-  #   text=commander.getMessage()
-  # )
-
-  try:
-    response = slack_client.chat_postMessage(
-      channel='#{}'.format(info["channel_name"]), 
-      text=commander.getMessage()
-    )#.get()
-  except SlackApiError as e:
-    logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
-    logging.error(e.response)
-    return make_response("", e.response.status_code)
-
-  return make_response("", response.status_code)
 
 @app.route("/slack/message", methods=["POST"])
 def handle_message():
@@ -97,10 +63,6 @@ def handle_message():
 # Start the Flask server
 if __name__ == "__main__":
   SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
-  SLACK_SIGNATURE = os.environ['SLACK_SIGNATURE']
   slack_client = WebClient(SLACK_BOT_TOKEN)
-  verifier = SignatureVerifier(SLACK_SIGNATURE)
-
-  commander = Slash("Hey there! It works.")
 
   app.run()
